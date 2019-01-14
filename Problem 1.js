341b9e7da5c54ef3c6083596c9cc56389ce13e72
Andrew Mooney <amooney@me.com>
2019-01-14 20:22:47 +1000

    Update Problem 1.js

--- Problem 1.js
+++ Problem 1.js
@@ -1,28 +1,3 @@
 "use strict"
-
-const multiplesThreeFive = (n) => {
-	let arr = []
-	for (let i = 0; i < n; i++) {
-		if (i % 3 === 0 || i % 5 ===0 ) {
-				arr.push(i)
-		}
-	}
-	return arr
-}
-
-const sumArray = (arr) => {
-	let sum = 0
-	arr.forEach((n) => {
-		sum += n
-	});
-	return sum
-}
-
-const arr = multiplesThreeFive(1000)
-const sum = sumArray(arr)
-
-console.log(sum)
-
-// Functional 1 liner
-let fsum = Array.apply(null, Array(1000)).map((x, i) => (i % 3 === 0 || i % 5 === 0) ? i : 0).reduce((total, num) => total + num)
-console.log(fsum)
+const sum = [...Array(1000).keys()].filter(n => n % 3 === 0 || n % 5 === 0).reduce((total, n) => total + n);
+console.log(sum);
